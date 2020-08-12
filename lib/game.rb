bd62c08becf62a1859e1228717c718b828300903
Yigit Mersin <yigitmersin@gmail.com>
2020-08-12 21:22:39 +0300

    Update win condition arrays & result_checker method

--- lib/game.rb
+++ lib/game.rb
@@ -1,21 +1,12 @@
-=begin
-outline & store winning posibilities
-store player 1 selection
-store player 2 selection
-if pass validity change turn to other player
-store selections
-check win or draw status
-    
-=end
 require_relative '../lib/board.rb'
 class Control
 attr_accessor :board,:turn,:win_parallel,:win_vertical,:win_cross,:cycle_end
  def initialize
     @@board = [1,2,3,4,5,6,7,8,9]
     @turn = false
-    @@win_parallel = [[1,2,3],[4,5,6],[7,8,9]]
-    @@win_vertical = [[1,4,7],[2,5,8],[3,6,9]]
-    @@win_cross = [[1,5,9],[3,5,7]]
+    @@win_parallel = [[1,2,3],[4,5,6],[7,8,9],[3,2,1],[6,5,4],[9,8,7]]
+    @@win_vertical = [[1,4,7],[2,5,8],[3,6,9],[7,4,1],[8,5,2],[9,6,3]]
+    @@win_cross = [[1,5,9],[3,5,7],[9,5,1],[7,5,3]]
     @@cycle_end = false
  end
 
@@ -63,9 +54,13 @@ end
   def result_checker(name_1, name_2)
     if (@@win_parallel + @@win_vertical+ @@win_cross).one?(user_1_selects) == true
        puts "#{name_1} win!"
+       update_board(input, turn)
+        print_board
        @@cycle_end = true
     elsif (@@win_parallel + @@win_vertical+ @@win_cross).one?(user_2_selects) == true
       puts "#{name_2} win!"
+      update_board(input, turn)
+        print_board
       @@cycle_end = true
     elsif @user_choices.length == 9
       puts "No winner!"
